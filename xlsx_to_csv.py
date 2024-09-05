6d5faf95dc1faf4a1a668c6f7896db068174b39b
Cupid_www <dearhoshino@gmail.com>
2024-09-05 19:36:05 +0800

    转换csv

--- xlsx_to_csv.py
+++ xlsx_to_csv.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-def xlsx_to_csv(path: str, index_col, output):
-    data = pd.read_excel(path, index_col)
-    data.to_csv(output, enconding='utf-8')
+def xlsx_to_csv(path: str, index, output):
+    data = pd.read_excel(path, index_col=index)
+    data.to_csv(output)
