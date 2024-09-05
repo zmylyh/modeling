740cf5a452c261393dd303723a75d483cc161d6a
ZMYLYH <125202100+zmylyh@users.noreply.github.com>
2024-09-05 21:53:50 +0800

    进展

--- main.py
+++ main.py
@@ -2,37 +2,42 @@ import pandas as pd
 from pulp import *
 
 # read data from csv file
-data1_1 = pd.read_csv('1_1.csv')
-data1_2 = pd.read_csv('1_2.csv')
-data2_1 = pd.read_csv('2_1.csv')
-data2_2 = pd.read_csv('2_2.csv')
+data1_1 = pd.read_csv('csv/1_1.csv')
+data1_2 = pd.read_csv('csv/1_2.csv')
+data2_1 = pd.read_csv('csv/2_1.csv')
+data2_2 = pd.read_csv('csv/2_2.csv')
 
 # condition
+cost_data = data2_2[['作物编号', '地块类型', '种植季次', '种植成本/(元/亩)']]
+production_data = data2_2[['作物编号', '地块类型', '种植季次', '亩产量/斤']]
 
+def split(price):
+    min, max = map(float, price.split('-'))
+    return pd.Series([min, max])
+data2_2[['min', 'max']] = data2_2['销售单价/(元/斤)'].apply(split)
+price_data = data2_2[['作物编号', '地块类型', '种植季次', 'min', 'max']]
 
+area_data = data1_1[['地块名称', '地块类型', '地块面积/亩']]
+plant_data = data1_2[['作物编号', '作物名称', '作物类型']]
 
-
-
-
+# create LP
 problem = LpProblem("Problem", LpMaximize)
 
-x1 = LpVariable("x1", 0)
-x2 = LpVariable("x2", 0)
+block = list(area_data['地块名称'])
+plant = list(plant_data['作物编号'])
+area = list(area_data['地块面积/亩'])
 
-x5 = LpVariable("x5", 0)
-x6 = LpVariable("x6", 0)
-x3 = 0.8 * x5
-x4 = 0.75 * x6
+combination = LpVariable.dict('comb', [(b, p) for b in block for p in plant], lowBound=0, cat='Continuous')
+for b in block:
+        problem += lpSum([combination[b,p] for p in plant]) <= area_data[area_data['地块名称'] == b]['地块面积/亩'].values[0]
 
-problem += 24 * x1 + 16 * x2 + 44 * x3 + 32 * x4 - 3 * x5 - 3 * x6
-
-problem += 4 * x1 + 3 * x2 + 4 * x5 + 3 * x6 <= 600
-problem += 4 * x1 + 2 * x2 + 6 * x5 + 4 * x6 <= 480
-problem += x1 + x5 <= 100
-
+# = sum 植物price*面积*亩产量 - 植物cost*面积
+problem += lpSum([production_data.query(f'作物编号=={p} & 地块类型 == {b}') for b in block for p in plant]*combination[b,p]*price_data.query(f'作物编号=={p} & 地块类型 == {b}')['max'] - cost_data.query(f'作物编号=={p} & 地块类型 == {b}')['种植成本/(元/亩)']*combination[b,p] for b in block for p in plant)
 problem.solve()
-
 print("Status: ", LpStatus[problem.status])
 print("Max z = ", value(problem.objective))
 for v in problem.variables():
     print(f'{v.name} = {v.varValue}')
+
+phd = cost_data.query('作物编号 == 1 & 地块类型 == "平旱地"')
+print(int(phd['种植成本/(元/亩)'].iloc[0]))
