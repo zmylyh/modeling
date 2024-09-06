a999f25553edaf35765739b9a2a4de94533ab433
Cupid_www <dearhoshino@gmail.com>
2024-09-06 15:35:38 +0800

    利润排序py

---
+++ 相对利润排序.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+data = pd.read_csv('2_2.csv')
+plant = data['作物编号']
+block = data['地块类型']
+cost = data['种植成本/(元/亩)']
+price = data['销售单价/(元/斤)']
+min_r, max_r = [], []
+for i in range(len(cost)):
+    min_r.append(round(float((price[i]).split('-')[0]) / float(cost[i]), 5))
+    max_r.append(round(float((price[i]).split('-')[-1]) / float(cost[i]), 5))
+
+data['min_r'] = min_r
+data['max_r'] = max_r
+
+data.sort_values(by='min_r', ascending=False).to_csv('min_relative.csv')
+data.sort_values(by='max_r', ascending=False).to_csv('max_relative.csv')
