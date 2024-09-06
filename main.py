c60a0be8a2eaf906cc2fcad20ec76e98d07f935f
ZMYLYH <125202100+zmylyh@users.noreply.github.com>
2024-09-06 09:47:15 +0800

    进展2

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from h5py.h5a import delete
 from pulp import *
 
 # read data from csv file
@@ -11,9 +12,12 @@ data2_2 = pd.read_csv('csv/2_2.csv')
 cost_data = data2_2[['作物编号', '地块类型', '种植季次', '种植成本/(元/亩)']]
 production_data = data2_2[['作物编号', '地块类型', '种植季次', '亩产量/斤']]
 
+
 def split(price):
     min, max = map(float, price.split('-'))
     return pd.Series([min, max])
+
+
 data2_2[['min', 'max']] = data2_2['销售单价/(元/斤)'].apply(split)
 price_data = data2_2[['作物编号', '地块类型', '种植季次', 'min', 'max']]
 
@@ -27,13 +31,40 @@ block = list(area_data['地块名称'])
 plant = list(plant_data['作物编号'])
 area = list(area_data['地块面积/亩'])
 
-combination = LpVariable.dict('comb', [(b, p) for b in block for p in plant], lowBound=0, cat='Continuous')
+combination = LpVariable.dict('comb', [str((b, p)) for b in block for p in plant], lowBound=0, cat='Continuous')
+print(combination["('A1', 1)"])
+print(combination)
+print(tuple("('A1', 1)"[1:-1].split(', '))[0])
+
+
+def simulation(t):
+    e1 = str(t[0])
+    return tuple((e1, int(t[1])))
+
+
 for b in block:
-        problem += lpSum([combination[b,p] for p in plant]) <= area_data[area_data['地块名称'] == b]['地块面积/亩'].values[0]
+    # limit the area
+    problem += (lpSum([combination[b, p] for p in plant])
+                <= area_data[area_data['地块名称'] == b]['地块面积/亩'].values[0])
+
+    # limit the plant 大白菜35，红萝卜37，白萝卜36,E1-E15,F1-F4
+    for i in range(15):
+        j = i + 1
+        area_name = f'E{j}'
+        for m in range(35,38):
+            del combination[str(simulation((area_name, m)))]
 
 # = sum 植物price*面积*亩产量 - 植物cost*面积
-problem += lpSum([production_data.query(f'作物编号=={p} & 地块类型 == {b}') for b in block for p in plant]*combination[b,p]*price_data.query(f'作物编号=={p} & 地块类型 == {b}')['max'] - cost_data.query(f'作物编号=={p} & 地块类型 == {b}')['种植成本/(元/亩)']*combination[b,p] for b in block for p in plant)
-problem.solve()
+# the best condition 1_1
+for b in block:
+    for p in plant:
+        problem += lpSum([int((production_data.query(f'作物编号=={p} & 地块类型 == {b}'))['亩产量/斤'].iloc[0])]
+                         * combination[simulation((b, p))]
+                         * int(price_data.query(f'作物编号=={p} & 地块类型 == {b}')['max'].iloc[0])
+                         - int(cost_data.query(f'作物编号=={p} & 地块类型 == {b}')['种植成本/(元/亩)'].iloc[0])
+                         * combination[simulation((b, p))])
+    problem.solve()
+
 print("Status: ", LpStatus[problem.status])
 print("Max z = ", value(problem.objective))
 for v in problem.variables():
